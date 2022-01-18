43ee1c974b2ebca3e0891b79b22fedf7ccd2c488
JeminHur <hurjem87@gmail.com>
2022-01-18 13:55:49 +0900

    const 추가

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,30 @@
 #include "Player.h"
+
+void Player::SetHP(int NewHP)
+{
+	if (NewHP >= 0)
+	{
+		HP = NewHP;
+	}
+}
+
+int Player::GetHP() const
+{
+	return HP;
+}
+
+void Player::Move()
+{
+}
+
+void Player::Collect()
+{
+}
+
+void Player::Run()
+{
+}
+
+void Player::Attack()
+{
+}
--- Player.h
+++ Player.h
@@ -1,11 +1,14 @@
 #pragma once
 class Player
 {
-public:
+private:
 	int HP;
 	int MP;
 	int Gold;
 
+public:
+	void SetHP(int NewHP);
+	int GetHP() const;
 	void Move();
 	void Collect();
 	void Run();
